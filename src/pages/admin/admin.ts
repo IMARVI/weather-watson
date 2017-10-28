5d54c7debfb3e3c422f9ec112cdf27cacca5b081
Martín Fernández <martin@martin-mac.local>
2017-10-28 09:17:35 -0500

    Estilos agregados

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 
 /**
  * Generated class for the AdminPage page.
@@ -8,18 +8,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+import { TarjetaModel } from '../../models/tarjeta-model'
+import { ClimaService } from '../../providers/clima-service/clima-service';
+
 @IonicPage()
 @Component({
   selector: 'page-admin',
   templateUrl: 'admin.html',
+    providers: [ClimaService, Platform],
 })
 export class AdminPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	datosClima: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,    public climaService: ClimaService,) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdminPage');
+  }
+
+  ngDoCheck() {
+    if(this.datosClima != null){
+      let nuevoClima = new TarjetaModel(
+        this.datosClima.display_location.full,
+        this.datosClima.feelslike_c,
+        this.datosClima.image.url,
+        this.datosClima.precip_today_metric,
+        this.datosClima.relative_humidity,
+        this.datosClima.wind_kph
+      );
+
+      this.datosClima = null;
+      //console.log(this.datosClima.display_location.city);
+      //console.log(this.datosClima);
+    }
   }
 
 }
